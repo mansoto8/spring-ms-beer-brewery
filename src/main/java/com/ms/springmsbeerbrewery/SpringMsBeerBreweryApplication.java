8f77582dbdf294f99bd96ab797fccda1b9aed644
juan <mansoto8@gmail.com>
2021-04-25 14:24:45 -0500

    First commit

---
+++ src/main/java/com/ms/springmsbeerbrewery/SpringMsBeerBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.ms.springmsbeerbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMsBeerBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMsBeerBreweryApplication.class, args);
+	}
+
+}
